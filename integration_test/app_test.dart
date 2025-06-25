b7456f7ffb9fff6373f28eb0563ac3896c87e660
Cesi21 <123073186+Cesi21@users.noreply.github.com>
2025-06-25 12:23:27 +0200

    Fix routine completion box type and add missing imports

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:planner/main.dart' as app;
 
